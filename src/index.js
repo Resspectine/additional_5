1595333fdcba5692200c156cc3e7c66be9d0392f
Resspectine <resspectyn@gmail.com>
2018-02-28 11:20:36 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    str = str.split('');
+    let opening = [];
+    let closing = [];
+    bracketsConfig.forEach(function (el) {
+        opening.push(el[0]);
+        closing.push(el[1]);
+    });
+    let stack = [];
+    let result = true;
+    str.forEach(function (eval) {
+        if (opening.indexOf(eval) !== -1) {
+            if (closing.indexOf(eval) !== -1 && stack[stack.length - 1] === eval) {
+                stack.pop();
+            } else {
+                stack.push(eval);
+            }
+        } else {
+            if (stack.pop() === opening[closing.indexOf(eval)]) {
+            } else {
+                result = false;
+            }
+        }
+    });
+    if (stack.length) {
+        result = false;
+    }
+    return result;
+};
